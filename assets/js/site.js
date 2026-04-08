e30c7fc10f1469d6a1a55b1c61cf5b4d4e163be9
ImpulsoWeb10 <rafagds130303@gmail.com>
2026-04-08 10:07:42 -0300

    Revert "Merge pull request #3 from ImpulsoWeb10/update/workspace-sync-2026-04-08"
    This reverts commit 59e9442364feaed6371a4de543f74f688ba731af, reversing
    changes made to 6e9fb291b53cd3842b3f6535c62b708a8ec5abb6.

--- assets/js/site.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  var images = document.querySelectorAll("img:not([loading])");
-  for (var i = 0; i < images.length; i += 1) {
-    images[i].setAttribute("loading", "lazy");
-    images[i].setAttribute("decoding", "async");
-  }
-})();
